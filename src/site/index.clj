979d8cd21bfccda83abbffc0117d9aaf0b9317f3
schmudde <d@schmud.de>
2020-04-14 16:48:18 +0200

    conform naming

--- src/site/index.clj
+++ src/site/index.clj
@@ -4,6 +4,16 @@
 (defn format-date [date]
   (if date (.format (java.text.SimpleDateFormat. "MMMM dd, yyyy") date)))
 
+(defn tags->links [tags]
+  [:ul.list.dib.pa0.ma0 {:class "tags"}
+   (map (fn [tag] [:li.dib.mr2 [:a.link {:href (str "/tags/" tag ".html")} tag]]) tags)])
+
+(defn list-posts [posts]
+  [:ul.tc.list.pl0
+   (for [post posts]
+     [:li.pv1
+      [:a.link {:href (:permalink post)}(:title post)]])])
+
 (defn head-template [global-meta post-title]
   [:head
    [:title (:site-title global-meta) (if post-title (str " | " post-title))]
@@ -12,7 +22,7 @@
    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
    [:link {:rel "stylesheet" :href "/css/tachyons.min.css"}]])
 
-(defn header [global-meta]
+(defn header-template [global-meta]
    [:nav
     [:a.link.black.b.dib.mr3 {:href "/" :title "Home"} (:site-title global-meta)]
     [:p.dib {:id "site-description"} (:description global-meta)]
@@ -21,11 +31,7 @@
      [:li.dib.mr2 [:a.link {:href "/about.html" :title "About"} "About"]]
      [:li.dib.mr2 [:a.link {:href "/feed.rss" :title "rss"} "RSS"]]]])
 
-(defn tags->links [tags]
-  [:span {:class "tags"}
-   (map (fn [tag] [:a.link.mr2 {:href (str "/tags/" tag ".html")} tag]) tags)])
-
-(defn render-post [post]
+(defn article-template [post]
   [:article
    [:header.tc
     [:h1 (:title post)]
@@ -33,23 +39,17 @@
      [:span (str ", tags: ")] (tags->links (:tags post))]]
    [:section.pv4 {:role "main"} (:content post)]])
 
-(defn list-posts [posts]
-  [:ul.tc.list.pl0
-   (for [post posts]
-     [:li.pv1
-      [:a.link {:href (:permalink post)}(:title post)]])])
-
 (defn body-template
   [global-meta page-title content]
   (page/html5 {:lang "en" :itemtype "http://schema.org/Blog"}
               (head-template global-meta page-title)
               [:body.avenir
-               [:header.pa5 (header global-meta)]
+               [:header.pa5 (header-template global-meta)]
                [:main.ph6 {:role "main"} content]]))
 
 (defn render-post-pages [{global-meta :meta post :entry}]
   (let [page-title (:title post)
-        content [:div (render-post post)]]
+        content [:div (article-template post)]]
     (body-template global-meta page-title content)))
 
 (defn render-tag-pages [{global-meta :meta tag :entry posts :entries}]
@@ -61,7 +61,7 @@
 (defn render-index-page [{global-meta :meta collection-meta :entry posts :entries}]
   (let [page-title "Home"
         content [:div
-                 (render-post (first posts))
+                 (article-template (first posts))
                  [:section.pa4 {:id "old-posts"}
                   [:h2.tc "Old Posts"]
                   (list-posts posts)]]]
