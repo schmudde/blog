9d93af5e7693af692380ba88c478c9edbd4523ca
schmudde <d@schmud.de>
2020-04-15 11:57:12 +0200

    tufte style and header

--- src/site/core.clj
+++ src/site/core.clj
@@ -5,14 +5,14 @@
   (if date (.format (java.text.SimpleDateFormat. "MMMM dd, yyyy") date)))
 
 (defn tags->links [tags]
-  [:ul.list.dib.pa0.ma0 {:class "tags"}
-   (map (fn [tag] [:li.dib.mr2 [:a.link {:href (str "/tags/" tag ".html")} tag]]) tags)])
+  [:ul.list.dib.ma0.pa0 {:class "tags"}
+   (map (fn [tag] [:li.dib.mr2 [:a {:href (str "/tags/" tag ".html")} tag]]) tags)])
 
 (defn list-posts [posts]
-  [:ul.tc.list.pl0
+  [:ul.list
    (for [post posts]
-     [:li.pv1
-      [:a.link {:href (:permalink post)}(:title post)]])])
+     [:li
+      [:a {:href (:permalink post)}(:title post)]])])
 
 (defn head-template [global-meta post-title]
   [:head
@@ -20,32 +20,35 @@
    [:meta {:charset "utf-8"}]
    [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
-   [:link {:rel "stylesheet" :href "/css/tachyons.min.css"}]])
+   [:link {:rel "stylesheet" :href "/css/tachyons.min.css"}]
+   [:link {:rel "stylesheet" :href "/css/tufte.min.css"}]])
 
 (defn header-template [global-meta]
-   [:nav
-    [:a.link.black.b.dib.mr3 {:href "/" :title "Home"} (:site-title global-meta)]
-    [:p.dib {:id "site-description"} (:description global-meta)]
+  [:nav
+   [:div ;; was here for .absolute
+    [:p.b.ma0.pa0.nowrap.f-headline.washed-red (:site-title global-meta)]]
+    #_[:a.b.dib.mr3 {:href "/" :title "Home"} (:site-title global-meta)]
+    [:p.dib.mb2 {:id "site-description"} (:description global-meta)]
     [:ul.list.ma0.pa0
-     [:li.dib.mr2 [:a.link {:href "/" :title "Home"} "Home"]]
-     [:li.dib.mr2 [:a.link {:href "/about.html" :title "About"} "About"]]
-     [:li.dib.mr2 [:a.link {:href "/feed.rss" :title "rss"} "RSS"]]]])
+     [:li.dib.mr2 [:a {:href "/" :title "Home"} "Home"]]
+     [:li.dib.mr2 [:a {:href "/about.html" :title "About"} "About"]]
+     [:li.dib.mr2 [:a {:href "/feed.rss" :title "rss"} "RSS"]]]])
 
 (defn article-template [post]
   [:article
-   [:header.tc
+   [:header
     [:h1 (:title post)]
     [:div [:time {:datetime (:date-published post) :pubdate "pubdate"} (format-date (:date-published post))]
      [:span (str ", tags: ")] (tags->links (:tags post))]]
-   [:section.pv4 {:role "main"} (:content post)]])
+   [:section {:role "main"} (:content post)]])
 
 (defn body-template
   [global-meta page-title content]
   (page/html5 {:lang "en" :itemtype "http://schema.org/Blog"}
               (head-template global-meta page-title)
-              [:body.avenir
-               [:header.pa5 (header-template global-meta)]
-               [:main.ph6 {:role "main"} content]]))
+              [:body
+               [:header (header-template global-meta)]
+               [:main {:role "main"} content]]))
 
 (defn render-post-pages [{global-meta :meta post :entry}]
   (let [page-title (:title post)
@@ -62,7 +65,7 @@
   (let [page-title "Home"
         content [:div
                  (article-template (first posts))
-                 [:section.pa4 {:id "old-posts"}
-                  [:h2.tc "Old Posts"]
+                 [:section {:id "old-posts"}
+                  [:h2 "Old Posts"]
                   (list-posts posts)]]]
     (body-template global-meta page-title content)))
