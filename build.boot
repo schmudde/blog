cb9aaadfef416302ef9ba4873d2396257b5578f6
schmudde <d@schmud.de>
2020-04-16 16:02:21 +0200

    fix post?/published? filters, metadata galore

--- build.boot
+++ build.boot
@@ -2,6 +2,7 @@
  :source-paths #{"src" "content"}
  :resource-paths #{"resources"}
  :dependencies '[[perun "0.4.3-SNAPSHOT" :scope "test"]
+                 [nrepl "0.7.0" :scope "test"]
                  [hiccup "1.0.5" :exclusions [org.clojure/clojure]]
                  [pandeiro/boot-http "0.8.3" :exclusions [org.clojure/clojure]]
                  [deraen/boot-livereload "0.2.1"]])
@@ -11,10 +12,14 @@
          '[deraen.boot-livereload :refer [livereload]])
 
 (defn page? [{:keys [original-path] :as meta}]
-  (some-> original-path (.startsWith "pages/")))
+  (if original-path
+    (.startsWith original-path "pages/")
+    false))
 
 (defn post? [{:keys [original-path] :as meta}]
-  (some-> original-path (.startsWith "posts/")))
+  (if original-path
+    (.startsWith original-path "posts/")
+    false))
 
 (defn published? [{:keys [date-published] :as meta}]
   (if date-published true false))
@@ -23,11 +28,11 @@
   (comp (perun/global-metadata :filename "site.base.edn")
         (perun/pandoc :cmd-opts ["--from" "markdown" "--to" "html5" "--filter" "pandoc-sidenote"])
         (perun/collection :renderer 'site.core/render-index-page :page "index.html"
-                          :filterer (and post? published?))
+                          :filterer (apply every-pred [post? published?]))
         (perun/render :renderer 'site.core/render-post-pages
-                      :filterer (and post? published?))
+                      :filterer (apply every-pred [post? published?]))
         (perun/tags :renderer 'site.core/render-tag-pages
-                    :filterer (and post? published?)
+                    :filterer (apply every-pred [post? published?])
                     :out-dir "public/tags")
 
         (perun/render :renderer 'site.core/render-post-pages :filterer page?)
@@ -65,3 +70,34 @@
         (perun/inject-scripts :scripts #{"js/livereload.js"})
         (livereload :asset-path "public" :filter #"\.(css|html|js)$")
         (serve :resource-root "public")))
+
+(comment
+
+  (published? {:date-published nil})
+  (published? {:date-published "avril 14th"})
+
+  (def path-data [{:original-path "posts/fefe"} {:original-path nil} {:original-path "po"} {:original-path "fee/fefef"} {:original-path "posts/zzz"} ])
+
+  (def pub-data [{:date-published "avril 14th"} {:date-published nil} {:date-published "may 14th"}])
+
+  (def pub-path-data [{:original-path "posts/fefe" :date-published "avril 14th"} {:original-path nil :date-published "date"} {:original-path "po" :date-published "may 14th"} {:original-path "fee/fefef" :date-published nil} {:original-path "posts/zzz" :date-published "may 14th"} ])
+
+  (filter post? path-data)
+  (filter published? pub-path-data)
+  (filterv (and post? published?) pub-path-data)
+  ; > ({:original-path "posts/fefe", :date-published "avril 14th"}
+  ;    {:original-path nil, :date-published "date"}
+  ;    {:original-path "po", :date-published "may 14th"}
+  ;    {:original-path "posts/zzz", :date-published "may 14th"})
+  (filter (or post? published?) pub-path-data)
+  ; > ({:original-path "posts/fefe", :date-published "avril 14th"}
+  ;    {:original-path "posts/zzz", :date-published "may 14th"})
+
+  (filter (apply every-pred [post? published?]) pub-path-data)
+  ; > ({:original-path "posts/fefe", :date-published "avril 14th"}
+  ;    {:original-path "posts/zzz", :date-published "may 14th"})
+
+  (map #(and (post? %) (published? %)) pub-path-data) ; (true false false false true)
+  (map #(or (post? %) (published? %)) pub-path-data) ; (true true true false true)
+
+  )
--- src/site/core.clj
+++ src/site/core.clj
@@ -9,64 +9,97 @@
    (map (fn [tag] [:li.dib.mr2 [:a {:href (str "/tags/" tag ".html")} tag]]) tags)])
 
 (defn list-posts [posts]
-  [:ul.list
+  [:ul.liste
    (for [post posts]
      [:li
       [:a {:href (:permalink post)}(:title post)]])])
 
-(defn head-template [global-meta post-title]
+(defn head-template [{:keys [site-title base-url author] :as global-meta}
+                     {:keys [title canonical-url] :as page-meta}]
   [:head
-   [:title (:site-title global-meta) (if post-title (str " | " post-title))]
+   [:title site-title (if title (str " | " title))]
    [:meta {:charset "utf-8"}]
    [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
+   [:meta {:name "twitter:card" :content "summary"}]
+   [:meta {:name "twitter:creator" :content "@dschmudde"}]
+   [:meta {:property "og:url" :content (or canonical-url base-url)}]
+   [:meta {:property "og:title" :content (or title site-title)}]
+   [:meta {:property "og:author" :content author}]
+   [:meta {:property "og:description":content (or (:description page-meta) (:description global-meta))}]
+   (if title ; if there is a post title, this is an article, otherwise it is a website
+     [:meta {:property "og:type":content "article"}]
+     [:meta {:property "og:type":content "website"}])
    [:link {:rel "stylesheet" :href "/css/tachyons.min.css"}]
    [:link {:rel "stylesheet" :href "/css/tufte.min.css"}]])
 
 (defn header-template [global-meta]
-  [:nav
-   [:div ;; was here for .absolute
-    [:p.b.ma0.pa0.nowrap.f-headline.washed-red (:site-title global-meta)]]
-    #_[:a.b.dib.mr3 {:href "/" :title "Home"} (:site-title global-meta)]
-    [:p.dib.mb2 {:id "site-description"} (:description global-meta)]
-    [:ul.list.ma0.pa0
-     [:li.dib.mr2 [:a {:href "/" :title "Home"} "Home"]]
-     [:li.dib.mr2 [:a {:href "/pages/now.html" :title "Now Page"} "Now"]]
-     [:li.dib.mr2 [:a {:href "/about.html" :title "About"} "About"]]
-     [:li.dib.mr2 [:a {:href "/feed.rss" :title "rss"} "RSS"]]]])
+  [:nav {:role "navigation" :itemscope "itemscope" :itemtype "https://schema.org/SiteNavigationElement"}
+   [:header {:itemscope "itemscope" :itemtype "https://schema.org/WPHeader"}
+    [:p.b.ma0.pa0.nowrap.f-headline.washed-red {:itemprop "publisher"} (:site-title global-meta)]
+    [:p.dib
+     [:span.mb2 {:id "site-description" :itemprop "about"} (:description global-meta)]
+     [:span.mb2 " by "]
+     [:span.mb2 {:itemprop "author"} (:author global-meta)]]]
+   [:ul.list.ma0.pa0
+    [:li.dib.mr2 [:a {:href "/" :title "Home"} "Home"]]
+    [:li.dib.mr2 [:a {:href "/pages/now.html" :title "Now Page"} "Now"]]
+    [:li.dib.mr2 [:a {:href "/about.html" :title "About"} "About"]]
+    [:li.dib.mr2 [:a {:href "/feed.rss" :title "rss"} "RSS"]]]])
 
 (defn article-template [post]
-  [:article
+  [:article {:itemscope "itemscope" :itemtype "http://schema.org/BlogPosting"}
+   [:meta {:itemprop "author" :content "David Schmudde"}]
+   #_[:link {:itemprop "mainEntityOfPage" :href (:canonical-url post)}]
    [:header
-    [:h1 (:title post)]
-    [:div [:time {:datetime (:date-published post) :pubdate "pubdate"} (format-date (:date-published post))]
+    [:h1 {:itemprop "headline"} (:title post)]
+    [:div [:time {:datetime (:date-published post) :pubdate "pubdate" :itemprop "datePublished"} (format-date (:date-published post))]
      [:span (str ", tags: ")] (tags->links (:tags post))]]
-   [:section {:role "main"} (:content post)]])
+   [:section {:role "main" :itemprop "articleBody"} (:content post)]])
 
 (defn body-template
-  [global-meta page-title content]
-  (page/html5 {:lang "en" :itemtype "http://schema.org/Blog"}
-              (head-template global-meta page-title)
+  [global-meta page-meta content]
+  (page/html5 {:lang "en" :itemscope "itemscope" :itemtype "http://schema.org/WebPage"}
+              (head-template global-meta page-meta)
               [:body
-               [:header (header-template global-meta)]
+               [:header {:itemscope "itemscope" :itemtype "https://schema.org/WPHeader"} (header-template global-meta)]
                [:main {:role "main"} content]]))
 
 (defn render-post-pages [{global-meta :meta post :entry}]
-  (let [page-title (:title post)
-        content [:div (article-template post)]]
-    (body-template global-meta page-title content)))
+  (let [content [:div (article-template post)]]
+    (body-template global-meta post content)))
 
 (defn render-tag-pages [{global-meta :meta tag :entry posts :entries}]
-  (let [page-title (str "Posts Tagged With \"" (:tag tag) "\"")
+  (let [tag (assoc tag :title (str "Posts Tagged With \"" (:tag tag) "\""))
         content [:div [:h1.tc (str "Posts Tagged As \"" (:tag tag) "\"" )]
                  (list-posts posts)]]
-    (body-template global-meta page-title content)))
+    (body-template global-meta tag content)))
 
 (defn render-index-page [{global-meta :meta collection-meta :entry posts :entries}]
-  (let [page-title "Home"
-        content [:div
+  (let [content [:div
                  (article-template (first posts))
                  [:section {:id "old-posts"}
                   [:h2 "Old Posts"]
                   (list-posts posts)]]]
-    (body-template global-meta page-title content)))
+    (body-template global-meta nil content)))
+
+(comment
+
+  (def global-data {:base-url "http://schmud.de/"
+                    :site-title "Beyond the Frame"
+                    :description "The metaphysics of information, art, and narrative"
+                    :author "David Schmudde"})
+  (def post-data {:canonical-url "✓" :title "✓" :description "✓" :tags ["tag-a" "tag-b"]})
+  (def tag-data {:canonical-url "✓" :title "✓" :description "✓" :tag "tag-a"})
+
+  (clojure.pprint/pprint (head-template global-data post-data))
+
+  (render-post-pages {:meta global-data :entry post-data})
+
+  (render-tag-pages {:meta global-data :entry tag-data})
+
+  (render-index-page {:meta global-data :entry nil :entries [post-data post-data post-data]})
+
+  (spit "temp.html" (render-index-page {:meta global-data :entry nil :entries [post-data post-data post-data]}))
+
+  )
